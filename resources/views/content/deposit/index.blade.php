df73a269d24add600a9986370ef3bbfa21f83aa1
erozzae <rosihann14@mail.ugm.ac.id>
2024-12-20 09:15:43 +0700

    revisi design list deposit status

--- resources/views/content/deposit/index.blade.php
+++ resources/views/content/deposit/index.blade.php
@@ -28,26 +28,7 @@
                             <td>{{ $item->user->nama_pemilik }}</td>
                             <td>{{ 'Rp ' . number_format($item->amount, 0, ',', '.') }}</td>
                             @if ($item->status === 'pending')
-                                <td>
-                                    <div class="dropdown">
-                                        <button type="button" class="btn btn-sm btn-primary me-1"
-                                            data-bs-toggle="dropdown">Pending</button>
-                                        <div class="dropdown-menu">
-                                            <form action="{{ route('deposit.update',  $item->id) }}" method="post">
-                                                @csrf
-                                                <input type="hidden" name="status" value="approved">
-                                                <button type="submit" class="btn btn-sm btn-success me-1"
-                                                   >Approved</button>
-                                            </form>
-                                            <form action="{{ route('deposit.update',  $item->id) }}">
-                                                @csrf
-                                                <input type="hidden" name="status" value="rejected">
-                                                <button type="submit" class="btn btn-sm btn-danger me-1"
-                                                    >Rejected</button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </td>
+                                <td><span class="btn btn-sm btn-primary me-1">Pending</span></td>
                             @elseif ($item->status == 'approved')
                                 <td><span class="btn btn-sm btn-success me-1">Approved</span></td>
                             @else
